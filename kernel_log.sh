11df08f4e682bc59d4b5063dda80266674f04a47
Ansam Nidal Mousa Abu Al Jadayel <137777479+Ansam-02@users.noreply.github.com>
2023-08-16 22:03:49 +0300

    Add files via upload

---
+++ kernel_log.sh
@@ -0,0 +1,23 @@
+
+
+#!/bin/bash
+
+# Get the current date and time in the required format
+CURRENT_DATETIME=$(date +"%Y-%m-%dT%H-%M")
+
+# Define filenames for log and compressed file
+LOG_FILENAME="kernel-logs-${CURRENT_DATETIME}.txt"
+COMPRESSED_FILENAME="kernel-logs-${CURRENT_DATETIME}.tar.bz2"
+
+# Extract kernel logs from the last hour and save to a text file
+journalctl --since "1 hour ago" -k > "$LOG_FILENAME"
+
+# Compress the text file using tar and bzip2
+tar -cjvf "$COMPRESSED_FILENAME" "$LOG_FILENAME"
+
+# Use dbxcli to upload the compressed file to Dropbox
+dbxcli put "$COMPRESSED_FILENAME"  "https://www.dropbox.com/home/LinuxProject"
+
+# Clean up temporary files
+rm "$LOG_FILENAME" "$COMPRESSED_FILENAME"
+
